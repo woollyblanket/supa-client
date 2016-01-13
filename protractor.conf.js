050da59f90b5657090009cc7024f05305cf86cf3
woollyblanket <amandadurkan@gmail.com>
2016-01-13 20:42:58 +0000

    Initial commit for ng2
    Using https://github.com/mgechev/angular2-seed as a starting point

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,27 +1,43 @@
-'use strict';
-
-var paths = require('./.yo-rc.json')['generator-gulp-angular'].props.paths;
-
-// An example configuration file.
 exports.config = {
-  // The address of a running selenium server.
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
-  //seleniumServerJar: deprecated, this should be set on node_modules/protractor/config.json
+    baseUrl: 'http://localhost:5555/dist/dev',
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  
-  baseUrl: 'http://localhost:3000',
+    specs: [
+        'dist/dev/**/*.e2e.js'
+    ],
+    exclude: [],
 
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
-  specs: [paths.e2e + '/**/*.js'],
+    framework: 'jasmine2',
 
-  // Options to be passed to Jasmine-node.
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
+    allScriptsTimeout: 110000,
+
+    jasmineNodeOpts: {
+        showTiming: true,
+        showColors: true,
+        isVerbose: false,
+        includeStackTrace: false,
+        defaultTimeoutInterval: 400000
+    },
+    directConnect: true,
+
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    onPrepare: function() {
+        var SpecReporter = require('jasmine-spec-reporter');
+        // add jasmine spec reporter
+        jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: true}));
+
+        browser.ignoreSynchronization = false;
+    },
+
+
+    /**
+     * Angular 2 configuration
+     *
+     * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
+     * `rootEl`
+     *
+     */
+    useAllAngular2AppRoots: true
 };
