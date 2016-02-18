a9f79eda46167d6a72a93b60946a28638a369e61
Mohamed Wasfi <mohamed.wusfi@gmail.com>
2016-02-18 13:59:35 +0000

    Proper search URL

--- app/services/results.ts
+++ app/services/results.ts
@@ -10,7 +10,8 @@ export class ResultsService {
 	// should return an observable that we subscribe to, rather than returning the 
 	// actual results.
 	getResults(): any {
-		return this.http.get('http://localhost:5555/app/assets/results.dummy.json')
+		//return this.http.get('http://localhost:5555/app/assets/results.dummy.json')
+		return this.http.get('http://tranquil-tundra-3993.herokuapp.com/search/chicken%7Cmilk')
 			.map(res => res.json());
 	}
 }
